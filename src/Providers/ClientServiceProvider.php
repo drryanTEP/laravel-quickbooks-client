12db3c546f96ee4e0a1e97e06221744c76846418
Ivan Lopez <ivanlopezvp@gmail.com>
2022-09-06 10:00:27 -0400

    fix error

--- src/Providers/ClientServiceProvider.php
+++ src/Providers/ClientServiceProvider.php
@@ -53,7 +53,7 @@ class ClientServiceProvider extends LaravelServiceProvider
                  if( request()->route('school') ) {
                      $school = School::find( request()->route('school') );
                 } else {
-                    $school = $app->auth->user->school();
+                    $school = auth()->user->getSchool();
                 }
 
                 $token = ($school->quickBooksToken)
